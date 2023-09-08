70a9cd344c90b69e5b84107c6d93910346a1dd85
moneyza001 <supalap.ss@gmail.com>
2023-09-08 10:00:20 +0700

    Add Theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div> Vite React Template</div>
-);
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  const styleObj = {
+    backgroundColor: isDarkMode ? "black" : "white",
+    color: isDarkMode ? "white" : "black",
+  };
+  return (
+    <div className="App" style={styleObj}>
+      <h1>Theme App</h1>
+      <button onClick={handleToggleTheme}>Toggle Theme</button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
